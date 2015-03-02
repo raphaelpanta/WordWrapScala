776fa90cfd5e376a6da08a9547e4570d2e1b58e4
unknown <rpspantaleao@GERTEC326.sefaz.se.gov.br>
2015-03-02 11:07:46 -0300

    Last test passed.

--- src/main/scala/com/github/raphaelpanta/linewrapper/Wrapper.scala
+++ src/main/scala/com/github/raphaelpanta/linewrapper/Wrapper.scala
@@ -4,5 +4,5 @@ object Wrapper {
 
   def wrap(text: String, maxColumn: Int): String =
     text.split(" ").reduceLeft(
-      (x, xs) => if (x.length() + xs.length() <= maxColumn) { x + " " + xs } else { x + "\n" + xs })
+      (x, xs) => if ((x.length()) % maxColumn + xs.length() >= maxColumn) { x + "\n" + xs } else { x + " " + xs })
 }
--- src/test/scala/com/github/raphaelpanta/unit/WrapperTest.scala
+++ src/test/scala/com/github/raphaelpanta/unit/WrapperTest.scala
@@ -9,16 +9,20 @@ import org.specs2.runner.JUnitRunner
 class WrapperTest extends Specification {
 
   "The string 'I love you'" should {
-    "be wrapped at column 6" in {
-      wrap("I love you", maxColumn = 6) must equalTo("I love\nyou")
+    "be wrapped at column 7" in {
+      wrap("I love you", maxColumn = 7) must equalTo("I love\nyou")
     }
     "be wrapped at column 10" in {
       wrap("I love you", maxColumn = 10) must equalTo("I love you");
     }
-    
+
     "be wrapped at column 1" in {
       wrap("I love you", maxColumn = 1) must equalTo("I\nlove\nyou");
     }
   }
-  
+  "The string 'Um pequeno jabuti xereta viu dez cegonhas felizes.'" should {
+    "be wrapped at column 20" in {
+      wrap("Um pequeno jabuti xereta viu dez cegonhas felizes.", 20) must equalTo("Um pequeno jabuti\nxereta viu dez\ncegonhas felizes.");
+    }
+  }
 }
